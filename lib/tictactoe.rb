15c4fd05ea224e9885f005aa77447bd4d3e8ace5
Nurulazrad Murad <nurulazrad@gmail.com>
2016-01-20 05:10:08 +0800

    Create playable human vs human game

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,3 +1,34 @@
 require_relative 'tictactoe/board'
 require_relative 'tictactoe/game'
 require_relative 'tictactoe/player'
+
+puts 'Select play mode:'
+puts '1. 1 Player'
+puts '2. 2 Players'
+players = gets.chomp.to_i
+
+if players == 1
+  p1 = Player.new 'Human', 'X'
+  p2 = Player.new 'Computer', 'O'
+else
+  p1 = Player.new 'Human #1', 'X'
+  p2 = Player.new 'Human #2', 'O'
+end
+
+puts 'Select board type:'
+puts '1. 3x3 board'
+puts '2. 4x4 board'
+puts '3. 5x5 board'
+
+board = gets.chomp.to_i
+case board
+when 2
+  board = 4
+when 3
+  board = 5
+else
+  board = 3
+end
+
+game = Game.new Board.new(board), [p1,p2]
+game.run
--- lib/tictactoe/game.rb
+++ lib/tictactoe/game.rb
@@ -5,6 +5,7 @@ class Game
     @board = board
     @turn = 0
     @current_player, @other_player = players
+    @game_over = false
   end
 
   def first_moves
@@ -13,5 +14,47 @@ class Game
 
   def switch_player
     @current_player, @other_player = @other_player, @current_player
+  end
+
+  def game_over?
+    @game_over
+  end
+
+  def show_board
+    @board.draw
+  end
+
+  def turn
+    show_board
+    puts "Player #{@current_player.name}, please enter a valid spot (#{@board.empty_fields.join(', ')}) to place your #{@current_player.marker}"
+    choice = gets.chomp.to_i
+
+    if @board.empty_fields.include? choice
+      @board.mark choice, @current_player.marker
+    else
+      puts "Please try again!"
+      turn
+    end
+    result
+    switch_player
+    turn unless game_over?
+  end
+
+  def result
+    if @board.winner? @current_player.marker
+      show_board
+      puts "Game Over! Winner is #{@current_player.name}"
+      @game_over = true
+    elsif @board.tie?
+      show_board
+      puts "Tie!"
+      @game_over = true
+    end
+  end
+
+  def run
+    while !game_over?
+      turn
+    end
   end
 end
